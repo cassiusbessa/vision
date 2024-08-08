7549b5416331d6bb55428a65feb3ea024091dd94
Cassius Queiroz Bessa <cassiusbessa@gmail.com>
2024-08-08 09:46:28 -0300

    refactor: :fire: remove comments

--- main.go
+++ main.go
@@ -39,19 +39,15 @@ func proxyRequest(c *gin.Context, target string) {
 		return
 	}
 
-	// Create a new reverse proxy
 	proxy := httputil.NewSingleHostReverseProxy(targetURL)
 
-	// Get the path prefix from the route
 	prefix := c.Param("proxyPath")
 	if strings.HasPrefix(prefix, "/") {
 		prefix = prefix[1:]
 	}
 
-	// Remove the prefix from the request path
 	newPath := strings.TrimPrefix(c.Request.URL.Path, "/"+prefix)
 
-	// Remove the trailing slash if it exists
 	if strings.HasSuffix(newPath, "/") && newPath != "/" {
 		newPath = newPath[:len(newPath)-1]
 	}
@@ -61,11 +57,9 @@ func proxyRequest(c *gin.Context, target string) {
 	c.Request.URL.Path = newPath
 	c.Request.Host = targetURL.Host
 
-	// Print debugging information
 	fmt.Printf("Original Path: %s\n", c.Request.URL.Path)
 	fmt.Printf("Forwarding to: %s%s\n", targetURL.String(), c.Request.URL.Path)
 	fmt.Printf("Full URL: %s\n", targetURL.ResolveReference(c.Request.URL).String())
 
-	// Serve the HTTP request
 	proxy.ServeHTTP(c.Writer, c.Request)
 }
