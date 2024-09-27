d89cb4012486d0a0a091dfa3a7997041ed48be58
cassius <cassiusbessa@gmail.com>
2024-09-27 11:10:59 -0300

    revert(vision-front): :recycle: add feed to home again

--- vision-front/src/app/page.tsx
+++ vision-front/src/app/page.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-// import Header from './components/header';
-// import { projectsMock } from './mocks';
-// import { ProjectPostContainer } from './components/project';
-// import NewPostInput from './components/input/new-post-input';
-// import CreateProjectModal from './components/modal/create-project-modal';
-import Profile from './profile/page';
+import Header from './components/header';
+import { projectsMock } from './mocks';
+import { ProjectPostContainer } from './components/project';
+import NewPostInput from './components/input/new-post-input';
+import CreateProjectModal from './components/modal/create-project-modal';
 
 export default function Home() {
-  // const user = {
-  //   fullName: 'John Doe',
-  //   image: 'https://i.pravatar.cc/150?img=68',
-  // };
+  const user = {
+    fullName: 'John Doe',
+    image: 'https://i.pravatar.cc/150?img=68',
+  };
   return (
-    // <div className="bg-base-200 flex flex-col items-center
-    // min-h-screen h-screen overflow-y-scroll">
-    //   <Header />
-    //   <NewPostInput user={user} className="mt-6" />
-    //   <CreateProjectModal />
-    //   <ProjectPostContainer projectsInfo={projectsMock} />
-    // </div>
-    <Profile />
+    <div className="bg-base-200 flex flex-col items-center
+    min-h-screen h-screen overflow-y-scroll"
+    >
+      <Header />
+      <NewPostInput user={user} className="mt-6" />
+      <CreateProjectModal />
+      <ProjectPostContainer projectsInfo={projectsMock} />
+    </div>
   );
 }
